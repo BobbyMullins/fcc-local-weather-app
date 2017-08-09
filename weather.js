a44f2ae85b93145d1fd568e3a32f1b2c06af9f54
Robert Mullins <robertmullins@192.168.1.2>
2017-08-09 14:31:43 -0400

    add geo location

--- weather.js
+++ weather.js
@@ -1,36 +1,39 @@
-    //Geo Location
-    var x = document.getElementById("geoLocation");
-    function getLocation() {
-        if (navigator.geolocation) {
-            x.innerHTML = navigator.geolocation.getCurrentPosition;
-        } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
+$(document).ready(function(){
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    
+          var api = "https://fcc-weather-api.glitch.me/api/current?";
+          var lat = 'lat=' + position.coords.latitude; 
+          var lon = 'lon=' + position.coords.longitude;   
+    
+          $.getJSON(api + lat + '&' + lon, function(data){
+              
+              //location
+              var displayLocation = document.getElementById('location');
+              displayLocation.textContent = 'Location: ' + data.name;
+            
+              //temperature
+              var displayTemp = document.getElementById('temp'); 
+              displayTemp.textContent = Math.floor(data.main.temp) + " " + "Degrees Celsius";
+            
+              //weather
+              var displayWeather = document.getElementById('weather'); 
+              displayWeather.textContent = data.weather.description;
+            
+              //wind speed
+              var displayWindSpeed = document.getElementById('windSpeed'); 
+              displayWindSpeed.textContent = 'Wind Speed: ' + ' ' + Math.floor(data.wind.speed) + " " + "MPH";
+            
+              //wind direction
+              var displayWindDirection= document.getElementById('windDirection'); 
+              displayWindDirection.textContent = data.wind.deg; 
+          })
+    
+       }); 
 
-$.getJSON('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139' , function(data){
-    console.log(data);
-    
-    //display location 
-    var displayLocation = document.getElementById('location');
-    displayLocation.textContent = data.name;
-    
-    //display temp
-    var displayTemp = document.getElementById('temp'); 
-    displayTemp.textContent = Math.floor(data.main.temp) + " " + "Degrees Celsius"; 
-    
-    //display weather
-    var displayWeather = document.getElementById('weather'); 
-    displayWeather.textContent = data.weather.description; 
-    
-    //display wind speed
-    var displayWindSpeed = document.getElementById('windSpeed'); 
-    displayWindSpeed.textContent = data.wind.speed; 
-    
-    //display wind direction
-    var displayWindDirection= document.getElementById('windDirection'); 
-    displayWindDirection.textContent = data.wind.deg; 
-    
+    } 
+  
 }); 
+
      
  
